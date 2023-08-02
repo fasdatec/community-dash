ef6748cdb51b84b9fcca7cb97003161feca158af
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2023-08-01 20:36:16 -0600

    Add commets to fix

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from './Pages/Login/Login'
+import Sign from'./Pages/Sign/Sign'
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/sign" element={<Sign />} />
+      </Routes>
+    </>
+  );
+}
+export default App
