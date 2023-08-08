b7ec0a1da5085ccc944cf3d6b064632b0d0761a4
sarahy19 <sarahyflo57@gmail.com>
2023-08-08 16:00:54 -0600

    the points of the text were removed for recharge the page

--- src/Components/Lazy/LazyCharge.jsx
+++ src/Components/Lazy/LazyCharge.jsx
@@ -4,7 +4,7 @@ import '../../assets/css/lazy.css'
 const LazyCharge = () => {
   return (
     <div className='commu__container__logo'>
-      <h1>ComuFasda...</h1>
+      <h1>ComuFasda</h1>
     </div>
   )
 }
