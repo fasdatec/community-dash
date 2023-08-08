fc123de789153717d12df69c8b1cd5fa1d30375b
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2023-08-08 17:48:01 -0600

    dasdasdsa

---
+++ src/Components/Lol/James.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const James = () => {
+  return (
+    <div>lol</div>
+  )
+}
+
+export default James
---
+++ src/Pages/Pagina.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import James from '../Components/Lol/James'
+const pagina = () => {
+  return (
+    <>
+        <James />
+    </>
+  )
+}
+
+export default pagina
