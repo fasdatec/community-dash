cc1caad2f9e017d72d033bc7943e614e57820e7d
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2023-10-22 02:27:09 -0600

    Estilado del Code

--- src/Components/PostSocialMedia/PostSocialMedia.jsx
+++ src/Components/PostSocialMedia/PostSocialMedia.jsx
@@ -5,15 +5,15 @@ import { ButtonYellowTransparentRound } from "../Buttons/Buttons";
 const PostSocialMedia = ({titleSocialMedia, imglogo}) => {
   return (
     <>
-    <div className={fasdatec.commu__card__social__media}>
-      <div className={fasdatec.commu__img__sm}>
-        <img className={fasdatec.commu__img__logo__sm} src={imglogo}/>
-        <p>{titleSocialMedia}</p>
+      <div className={fasdatec.commu__card__social__media}>
+        <div className={fasdatec.commu__img__sm}>
+          <img className={fasdatec.commu__img__logo__sm} src={imglogo}/>
+          <p>{titleSocialMedia}</p>
+        </div>
+        <div className={fasdatec.commu__button__card}>
+          <ButtonYellowTransparentRound titleButton='Crear'/>
+        </div>
       </div>
-      <div className={fasdatec.commu__button__card}>
-        <ButtonYellowTransparentRound titleButton='Crear'/>
-      </div>
-    </div>
     </>
   )
 }
