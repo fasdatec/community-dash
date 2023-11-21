94b2de835a9d8b1a527d57d84330aae88e00a057
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2023-11-20 20:43:57 -0600

    Done userssssssssssssssssssssssssssss

--- src/assets/helpers/routes.js
+++ src/assets/helpers/routes.js
@@ -11,7 +11,7 @@ const routes = {
         listado: '/listpost/',
         creacion: '/createpost/',
         tips: '/tipspost/',
-        calendar: '/calendar/',
+        calendar: '/home/calendar/',
         suscripciones: '/subscriptions/'
     } 
 
