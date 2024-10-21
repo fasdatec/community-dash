94c9d51fac2e24a8a14410bbc92a347eb8732c29
David <dm987257@gmail.com>
2024-10-21 16:06:53 -0600

    add styles in subscriptions

--- src/Components/PlansSubs/PlansSubs.jsx
+++ src/Components/PlansSubs/PlansSubs.jsx
@@ -26,8 +26,18 @@ const PlansSubs = () =>{
                             <li>ejemplo 3</li>
                             </ul>}
                         buttonPlan={<ButtonYellowRound titleButton='Continuar gratis' direction='/Dashboard' />}/>
-                        <CardSubs titlePlan='Basic' 
-                        pricePlan='$9.00 USD'
+                        <CardSubs titlePlan='Pro' 
+                        pricePlan='$18.00 USD'
+                        describePlan='Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
+                        advantagePlan={<ul>
+                            <li>ejemplo 1</li>
+                            <li>ejemplo 2</li>
+                            <li>ejemplo 3</li>
+                            </ul>} 
+                        buttonPlan={<stripe-buy-button className={fasdatec.commu__prueba} buy-button-id="buy_btn_1OAhJ7Aoi5Crq5LdbDQtYZ4u" publishable-key="pk_test_51O9rtYAoi5Crq5LdBzZwXeYZSY5CbBVGfLqk4OmgBwkos7PYA6UHbQum2k24L3ela4Ytti5HtADCClv3ZHth671r00I0NoiYjC">
+                        </stripe-buy-button>} />
+                        <CardSubs titlePlan='Expert' 
+                        pricePlan='$40.00 USD'
                         describePlan='Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
                         advantagePlan={<ul>
                             <li>ejemplo 1</li>
