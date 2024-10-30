ee43e1e97c35173f03e676df755e8c13eb6f8c0a
David <dm987257@gmail.com>
2024-10-30 15:58:59 -0600

    I made graphic changes in subscriptions, in publications I added new posts, in calendar I changed the background color, in post creation I added button styles and in clients I made graphic changes. -David Mendoza

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -6,19 +6,19 @@ const Post = () => {
   return (
     <>
     <div className={fasdatec.commu__post}>
-      <FacebookEmbed className ={fasdatec.commu__post__facebook} url="https://www.facebook.com/100077404405612/posts/pfbid036jtaZUWSAJLaeYuApzzFeMdx9dRRa9mH1c5AusoCmjs4ep1i6ss2UjQW9WmtB1XEl/?mibextid=9R9pXO" width={328} />
-      <InstagramEmbed url="https://www.instagram.com/p/CyOq3mHpRD3/?igshid=MTc4MmM1YmI2Ng==" width={328} />
-      <TikTokEmbed url="https://www.tiktok.com/@espinoza19740/video/7246694024986283270" width={325} />
-      <TwitterEmbed url="https://twitter.com/CondorPana/status/1713718363904454746?ref_src=twsrc%5Etfw" width={325} />
-      <YouTubeEmbed url="https://www.youtube.com/embed/Djgw-uR2n34?si=ujTjlGp4qQVnmd3D" width={325} height={220} />
+      <FacebookEmbed className ={fasdatec.commu__post__facebook} url="https://www.facebook.com/Formula1/posts/pfbid02iQmC76nYo31NUae7THwjdHYZyWKN8vLfkuaXUbZ4QEDma59y4mEfKQbPBKzjNyU8l?rdid=32c6CpK1UBWOrTbZ" width={328} />
+      <InstagramEmbed url="//www.instagram.com/p/C9nn7VcxmIP/" width={328} />
+      <TikTokEmbed url="https://www.tiktok.com/@fasdatec/video/7431257603348417798" width={325} />
+      <TwitterEmbed url="https://x.com/F1/status/1851605414409916662" width={325} />
+      <YouTubeEmbed url="https://youtu.be/hHJH-CEWH3U?si=X37Qu5bMIkQI6vrO" width={325} height={220} />
       <LinkedInEmbed 
     url="https://www.linkedin.com/embed/feed/update/urn:li:share:6898694772484112384"
     postUrl="https://www.linkedin.com/posts/peterdiamandis_5-discoveries-the-james-webb-telescope-will-activity-6898694773406875648-z-D7"
     width={325}
     height={570} 
   />
-    </div>
+    </div>          
     </>
   )
 }
-export default Post
+export default Post 
